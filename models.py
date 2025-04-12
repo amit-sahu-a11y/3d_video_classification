ccd201ca9cd3953b56f9474d27ffd5574a69926b
amit-sahu-a11y <sahuamit4783@gmail.com>
2025-04-13 00:46:57 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,27 @@
+import torch.nn as nn
+
+class Simple3DCNN(nn.Module):
+    def __init__(self, num_classes):
+        super(Simple3DCNN, self).__init__()
+
+        self.features = nn.Sequential(
+            nn.Conv3d(3, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool3d(kernel_size=2, stride=2),
+
+            nn.Conv3d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool3d(kernel_size=2, stride=2),
+
+            nn.Conv3d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.AdaptiveAvgPool3d((1, 1, 1))
+        )
+
+        self.classifier = nn.Linear(256, num_classes)
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
